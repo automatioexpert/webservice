fee5ec87f1effe40d87e5846c1ab92ccc5af04a1
Valmiki Vishwakarma <you@example.com>
2022-12-19 22:33:41 +0530

    add.

---
+++ webservice/webservice/src/main/java/com/webservice/services/URLBuilder.java
@@ -0,0 +1,8 @@
+package com.webservice.services;
+
+public class URLBuilder {
+	
+	public final static  String loginurl = "http://www.fashionandyou.com/api/login/business?operationType=login";
+	public final static String allproduct ="http://www.fashionandyou.com/search-new/web/menu?ptype=all";
+
+}
